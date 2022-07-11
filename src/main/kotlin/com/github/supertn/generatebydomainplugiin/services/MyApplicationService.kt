d7c156aa650be8336216b43fd4dc9fc6b7c320a8
GitHub Action <action@github.com>
2022-07-11 10:22:58 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/supertn/generatebydomainplugiin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.supertn.generatebydomainplugiin.services
+
+import com.github.supertn.generatebydomainplugiin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
